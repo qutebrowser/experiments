75ba46b7d1f3559fe8bb1a12a707d5a1456581a4
Florian Bruhin <me@the-compiler.org>
2022-04-10 19:08:46 +0200

    Qt 6: Handle QtOpenGL move

--- qutebrowser/qt/gui.py
+++ qutebrowser/qt/gui.py
@@ -3,10 +3,12 @@ from qutebrowser.qt import machinery
 
 if machinery.use_pyqt5:
     from PyQt5.QtGui import *
+    del QOpenGLVersionProfile  # moved to QtOpenGL in Qt 6
 elif machinery.use_pyqt6:
     from PyQt6.QtGui import *
 elif machinery.use_pyside2:
     from PySide2.QtGui import *
+    del QOpenGLVersionProfile  # moved to QtOpenGL in Qt 6
 elif machinery.use_pyside6:
     from PySide6.QtGui import *
 else:
